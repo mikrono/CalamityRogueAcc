738f2b133e78372d100610c9a824864435415b73
mikrono <qse365@gmail.com>
2024-06-04 11:06:21 +0900

    Add Dager Charm function

--- Common/ModPlayers/AccessoryPlayer.cs
+++ Common/ModPlayers/AccessoryPlayer.cs
@@ -9,6 +9,9 @@ using CalamityMod;
 using CalamityMod.Buffs.DamageOverTime;
 using CalamityMod.Buffs.StatDebuffs;
 using static Terraria.ModLoader.ModContent;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using CalamityMod.Projectiles.Rogue;
 
 namespace CalamityRogueAcc.Common.ModPlayers
 {
@@ -25,6 +28,26 @@ namespace CalamityRogueAcc.Common.ModPlayers
             leadCore = false;
             Corrupted_Fang = false;
             Dagger_Charm = false;
+        }
+
+        public override bool Shoot(Item item, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            if (item.CountsAsClass<RogueDamageClass>())
+            {
+                if (Player.Calamity().StealthStrikeAvailable() && Dagger_Charm)
+                {
+                    const int knifeCount = 5;
+                    const int knifeDamage = 50;
+                    Vector2 BaseVelocity = new(15f * Player.direction, 0);
+
+                    for (int i = 0; i < knifeCount; i++)
+                    {
+                        Vector2 newVelocity = BaseVelocity.RotatedBy(MathHelper.ToRadians(3 * (i - 2)));
+                        Projectile.NewProjectile(source, Player.Center, newVelocity, ModContent.ProjectileType<VeneratedKnife>(), knifeDamage, 0f, Player.whoAmI);
+                    }
+                }
+            }
+            return true;
         }
 
         public void NPCDebuffs(NPC target, bool melee, bool ranged, bool magic, bool summon, bool rogue, bool whip, bool proj = false, bool noFlask = false)
